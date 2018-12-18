a508ab143364539b3c01d5b91b5cb8df4d35cf09
Sylarair <qwang601@gmail.com>
2018-12-18 12:00:27 -0500

    Update get_ucsc_screenshots.sh

--- get_ucsc_screenshots.sh
+++ get_ucsc_screenshots.sh
@@ -1,29 +1,30 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Author: Qin Wang
-# Usage ucsc_screenshots.sh hublink bed_file outpath
-# Usage ucsc_screenshots.sh https://***/hub.txt Peak.bed Your_outpath
+# Usage: ucsc_screenshots.sh hublink bed_file Your_outpath
+# Example: ucsc_screenshots.sh https://***/hub.txt narrowPeak.bed /home/***/***
 # hublink is what you put into the UCSC browser(the hub.txt contains all the tracks you have loaded)
-#example:
-#https://genome.ucsc.edu/cgi-bin/hgTracks?db=mm10&position=chr8:126820533-126833569&hubClear=https://users.wenglab.org/***/***/***/hub.txt
+# hublink example: https://users.wenglab.org/***/***/***/hub.txt
 
-hublink=$1
+hublink=$1 
 infile=$2
 outpath=$3
 # db is the genome
 db=mm10
 
-### your bed file has a 4 columns format like: chr1 1000000 1010000 peak-1
+### your bed file has a 4 columns at least, like: chr1 1000000 1010000 peak-1
 ranges=( $(cut -f1-4 ${infile} | awk '{print $1":"$2"-"$3"."$4}') )
 #echo $ranges
 for i in "${ranges[@]}"; do
     # position="chr1:1000000-1010000"
     position=$(echo $i | cut -d. -f1)
     # position=$(echo $i)
-    echo ${i}
+    #echo ${i}
     ### using the 4th column to name pdf files
     outpdf=${outpath}"/"$(echo $i | cut -d. -f2).pdf
+    #https://genome.ucsc.edu/cgi-bin/hgTracks?db=mm10&position=chr8:126820533-126833569&hubClear=https://users.wenglab.org/***/***/***/hub.txt
     curl -s "https://genome.ucsc.edu/cgi-bin/hgTracks?db=$db&position=$position&hubClear=$hublink" >/dev/null
+    #https://genome.ucsc.edu/cgi-bin/hgTracks?db=mm10&position=chr8:126820533-126833569&hubClear=https://users.wenglab.org/***/***/***/hub.txt&hgt.psOutput=on
     pdf_page="https://genome.ucsc.edu/cgi-bin/hgTracks?db=$db&position=$position&hubClear=$hublink&hgt.psOutput=on"
     pdf_url=$(curl -s "$dl_page" | grep "the current browser graphic in PDF" | grep -E -o "\".+\"" | tr -d "\"" | sed 's/../https:\/\/genome.ucsc.edu/')
 
